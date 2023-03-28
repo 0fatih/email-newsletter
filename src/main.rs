39b27b927b7bcc7bfdb13bcd9e26dda640fb5743
Fatih <fatihfurkn@gmail.com>
2023-03-28 20:55:58 +0300

    add serde and type definition for subscribe

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use email_newsletter::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("http://127.0.0.1:8080")?;
+    run(listener)?.await
 }
