42ca2f7f922db8bad518a083b668a0a8510e989f
Fatih <fatihfurkn@gmail.com>
2023-03-30 13:20:11 +0300

    add ci; add db

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::net::TcpListener;
 
-use email_newsletter::startup::run;
+use email_newsletter::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("http://127.0.0.1:8080")?;
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let listener = TcpListener::bind(format!(
+        "http://127.0.0.1:{}",
+        configuration.application_port
+    ))?;
     run(listener)?.await
 }
