c00f54c603df89097545ec9bf2fe0845ab34bedf
Fatih <fatihfurkn@gmail.com>
2023-03-31 11:34:25 +0300

    save user to the db

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::net::TcpListener;
 
 use email_newsletter::{configuration::get_configuration, startup::run};
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let listener = TcpListener::bind(format!(
         "http://127.0.0.1:{}",
         configuration.application_port
     ))?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
