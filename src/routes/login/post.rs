86f4016dd5152eaf94c3604ec322a5e458c15b0e
Fatih <fatihfurkn@gmail.com>
2023-04-16 14:18:21 +0300

    📦 chore(Cargo.toml): add urlencoding dependency ✨ feat(login): add error handling to login form and redirect to login page with error message The urlencoding dependency was added to the project. The login form now accepts a query parameter 'error' which is used to display an error message to the user. The login_form function was updated to handle the query parameter and display the error message if it exists. The LoginError struct was updated to implement the ResponseError trait, which allows it to return a custom error response. The error_response function was implemented to redirect to the login page with the error message encoded in the query parameter. The status_code function was also updated to return a 303 See Other status code.

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,5 @@
 use actix_web::http::header::LOCATION;
-use actix_web::{web, HttpResponse, ResponseError};
+use actix_web::{http::StatusCode, web, HttpResponse, ResponseError};
 use secrecy::Secret;
 use sqlx::PgPool;
 
@@ -28,11 +28,16 @@ impl std::fmt::Debug for LoginError {
 }
 
 impl ResponseError for LoginError {
-    fn status_code(&self) -> reqwest::StatusCode {
-        match self {
-            LoginError::AuthError(_) => reqwest::StatusCode::UNAUTHORIZED,
-            LoginError::UnexpectedError(_) => reqwest::StatusCode::INTERNAL_SERVER_ERROR,
-        }
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        let encoded_error = urlencoding::Encoded::new(self.to_string());
+
+        HttpResponse::build(self.status_code())
+            .insert_header((LOCATION, format!("/login?error={}", encoded_error)))
+            .finish()
+    }
+
+    fn status_code(&self) -> StatusCode {
+        StatusCode::SEE_OTHER
     }
 }
 
